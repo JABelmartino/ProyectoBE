0ca863da2d25e185703f6182f482bbd34f17a63d
Julian Belmartino <belmartinojulian@gmail.com>
2022-08-04 20:46:19 -0300

    Desafio 4

--- contenedor.js
+++ contenedor.js
@@ -4,11 +4,11 @@ class Contenedor{
     constructor(ruta){
         this.ruta = ruta
     }
-    async getRandom(){
+    async getProductos(){
         try{
             let dataArchivo = await fs.promises.readFile(this.ruta, 'utf-8')
             let dataArchivoParse = JSON.parse(dataArchivo)
-            let producto = dataArchivoParse[Math.floor(Math.random()*dataArchivoParse.length)];
+            let producto = dataArchivoParse
             if(producto){
                 return producto               
             }else{
@@ -19,14 +19,51 @@ class Contenedor{
             console.log(error)
         }
     }
-    async getAll(){
+    async updateById(obj, id){
+        try{
+            console.log(obj)
+            let dataArchivo = await fs.promises.readFile(this.ruta, 'utf-8')
+            let dataArchivoParse = JSON.parse(dataArchivo)
+            const objindex = dataArchivoParse.find(prod => prod.id == id)
+            console.log(objindex)
+            if(objindex){
+                await fs.promises.writeFile(this.ruta,JSON.stringify(obj, null, 1))  
+            }else{
+                return {error: 'No hay productos con ese Id'}
+            }    
+           }catch(error){
+            console.log(error)
+        }
+    }
+    
+    async getId(id){
         try{
             let dataArchivo = await fs.promises.readFile(this.ruta, 'utf-8')
             let dataArchivoParse = JSON.parse(dataArchivo)
-            if(dataArchivoParse.length){
-                   return dataArchivoParse
+            console.log(id)
+            const objIndex = dataArchivoParse.find(prod => prod.id == id)
+            console.log(objIndex)
+            if(objIndex){
+                return objIndex
             }else{
-                console.log('No hay productos')
+                return {error: 'No hay productos con ese Id'}
+            }    
+           }catch(error){
+            console.log(error)
+        }
+    }
+    
+    async deleteById(id){
+        try{
+            let dataArchivo = await fs.promises.readFile(this.ruta, 'utf-8')
+            console.log(id)
+            let dataArchivoParse = JSON.parse(dataArchivo)
+            const objindex = dataArchivoParse.filter(prod => prod.id != id)
+            console.log(objindex)
+            if(objindex){
+                await fs.promises.writeFile(this.ruta,JSON.stringify(objindex))
+            }else{
+                return {error: 'No hay productos con ese ID'}
             }    
            }catch(error){
             console.log(error)
--- server.js
+++ server.js
@@ -1,23 +1,70 @@
 const express = require('express')
+const { Router } = express
 const Contenedor = require("./contenedor");
 
 const contenedor = new Contenedor('./productos.txt')
 
 const app = express()
+const routerProductos = Router()
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hola mundo</h1>')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+
+const arrayProductos = []
+///-Productos-///
+routerProductos.get('/', async (req, res) => {
+    const productos = await contenedor.getProductos()
+    res.json({
+        productos
+    })
+})
+/*
+routerProductos.get('/:id', async (req, res) => {
+    const {id} = req.params
+    const elegido = await contenedor.getId(id)
+    if(elegido){
+     res.json({elegido})
+    }else{
+        return {error:'No existe'}
+    }
+ })
+*/
+routerProductos.post('/', (req, res) => {
+    const {title,price} = req.body
+    const productos = arrayProductos.push({title,price})
+    res.json({
+        productos:arrayProductos
+    })
 })
 
-app.get('/productos', (request, response) => {
-    contenedor.getAll().then(array => response.send(array))   
+routerProductos.put('/:id', (req, res) => {
+    const {id} = req.params
+    const {title,price,thumbnail} = req.body
+    const obj = {title,price,thumbnail}
+    const actualizado = contenedor.updateById(obj,id)
+    res.json({
+        actualizado
+    })
 })
 
-app.get('/productoRandom', (request, response) => {
-    contenedor.getRandom().then(array => response.send(array))   
+routerProductos.delete('/:id',async (req, res) => {
+    const {id} = req.params
+    const borrado = await contenedor.deleteById(id)
+        res.json({
+            mensaje: 'borrado existosamente',
+            id
+        })
 })
 
-const PORT = 8080
+
+
+app.use('/api/productos', routerProductos)
+
+
+
+
+
 const server = app.listen(8080, () =>{
     console.log(server.address().port)
 })
